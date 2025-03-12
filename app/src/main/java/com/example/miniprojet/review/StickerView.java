560e04c5a3c89a801a6523e99393cc3cc59b39ab
mamadou-25 <bahmamadou524@gmail.com>
2025-03-12 07:04:28 +0100

    zoom stickers implemented #24

--- app/src/main/java/com/example/miniprojet/review/StickerView.java
+++ app/src/main/java/com/example/miniprojet/review/StickerView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import androidx.appcompat.widget.AppCompatImageView;
@@ -18,19 +19,21 @@ public class StickerView extends AppCompatImageView implements View.OnTouchListe
     private final List<StickerInfo> stickerInfos = new ArrayList<>();
     private final PointF initialTouchPoint = new PointF();
     private int touchedStickerIndex = -1;
+    private ScaleGestureDetector scaleGestureDetector;
 
     public StickerView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public StickerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
-    private void init() {
+    private void init(Context context) {
         setOnTouchListener(this);
+        scaleGestureDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
     @Override
@@ -50,28 +53,34 @@ public class StickerView extends AppCompatImageView implements View.OnTouchListe
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                initialTouchPoint.set(event.getX(), event.getY());
-                touchedStickerIndex = findTouchedSticker(event.getX(), event.getY());
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (touchedStickerIndex != -1) {
-                    float dx = event.getX() - initialTouchPoint.x;
-                    float dy = event.getY() - initialTouchPoint.y;
-                    stickerInfos.get(touchedStickerIndex).getMatrix().postTranslate(dx, dy);
+        scaleGestureDetector.onTouchEvent(event);
+        if (!scaleGestureDetector.isInProgress()) {
+            int action = event.getActionMasked();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
                     initialTouchPoint.set(event.getX(), event.getY());
-                    invalidate();
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                touchedStickerIndex = -1;
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + event.getAction());
+                    touchedStickerIndex = findTouchedSticker(event.getX(), event.getY());
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (touchedStickerIndex != -1) {
+                        float dx = event.getX() - initialTouchPoint.x;
+                        float dy = event.getY() - initialTouchPoint.y;
+                        stickerInfos.get(touchedStickerIndex).getMatrix().postTranslate(dx, dy);
+                        initialTouchPoint.set(event.getX(), event.getY());
+                        invalidate();
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    touchedStickerIndex = -1;
+                    break;
+                default:
+                    break;
+            }
         }
         return true;
     }
+
 
     private int findTouchedSticker(float x, float y) {
         for (int i = stickerInfos.size() - 1; i >= 0; i--) {
@@ -89,5 +98,19 @@ public class StickerView extends AppCompatImageView implements View.OnTouchListe
         info.getMatrix().invert(inverse);
         inverse.mapPoints(pts);
         return pts[0] >= 0 && pts[0] < info.getBitmap().getWidth() && pts[1] >= 0 && pts[1] < info.getBitmap().getHeight();
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            if (touchedStickerIndex != -1) {
+                StickerInfo info = stickerInfos.get(touchedStickerIndex);
+                info.setWidth((int) (info.getWidth() * detector.getScaleFactor()));
+                info.setHeight((int) (info.getHeight() * detector.getScaleFactor()));
+                info.setScaleFactor(info.getScaleFactor() * detector.getScaleFactor());
+                invalidate();
+            }
+            return true;
+        }
     }
 }
